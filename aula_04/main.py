313f9c0fafc92a65219f44c089dcb501187f9b92
Julio Pereira <juliopereira.dev@gmail.com>
2024-01-11 20:16:10 -0300

    aula 04 parte 01

---
+++ aula_04/main.py
@@ -0,0 +1,15 @@
+
+import random as rd
+
+#GERENCIADOR DE PACOTES => PIP
+# import tqdm as tq
+from tqdm import tqdm
+
+for _ in tqdm(range(1000000000), "Contando até 1.000.000.000 =>"):
+    pass
+
+# for _ in range(5):
+#     print("*")
+
+# VIRTUAL ENV
+
---
+++ aula_04/modulo_03.py
@@ -0,0 +1,6 @@
+"""
+    Laboratório 1
+    
+    Refazer o exercício relativo ao Caixa Eletrônico, considerando que o valor de cada cédula permanece armazenado
+    em uma lista, ao invés de ser mostrado na tela.
+"""
