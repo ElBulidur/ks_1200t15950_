fb00cc15f67eb7201197baee11c9cae853105a8b
Julio Pereira <juliopereira.dev@gmail.com>
2024-01-15 18:36:12 -0300

    inicio aula 06

---
+++ aula_06/exercicios.py
@@ -0,0 +1,43 @@
+##### VARIÁVEIS E COLEÇÕES #########
+
+""" 
+    1 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+
+    2 - Crie um programa que leia dois números e mostre a soma entre eles.
+
+    3 - faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+    4 - Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+    5 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+    6 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+    7 - Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada (Usa o loop de repetição).
+
+    8 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares 
+    ela pode comprar (Veja a cotação).
+
+    9 - Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+    
+    10 - Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+    Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido. (use lista e o modulo random)
+
+    11 - Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. 
+    Ex: Digite um número: 1834 unidade: 4 dezena: 3 centena: 8 milhar: 1
+
+    12 - Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra ‘A’.
+
+    13 - Escreva um programa que leia a velocidade de uma carro. Se ele ultrapassar 80 km/h, 
+    mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por cada Km acima do limite.
+
+    14 - Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.
+
+    15 - Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+    
+    16 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
+    Para salários superiores a R$ 1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+    
+    17 - Escreva um programa que leia dois números inteiros e os compare mostrando na tela a seguinte mensagem:
+    O primeiro valor é maior, o segundo valor é maior ou Não existe valor maior, os dois são iguais.
+"""
